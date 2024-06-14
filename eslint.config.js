da5c32e0af0d99629866713e171d6c93a68cad3d
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2024-06-14 09:28:13 +0700

    update 7 files and delete 1 file

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,14 @@
-import config from 'eslint-config-standard';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default [...[].concat(config)];
+export default [
+	{ files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	...pluginVue.configs['flat/essential'],
+	eslintPluginPrettierRecommended,
+];
