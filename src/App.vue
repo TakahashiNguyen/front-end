14778731cb54e19d2ffad3eb23e5cd060a737b98
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2024-06-14 11:29:11 +0700

    update App.vue and BootstrapTest.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+	import { ref } from 'vue';
+
+	const isDarkMode = ref(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+	export default {
+		setup() {
+			const toggleDarkMode = () => {
+				isDarkMode.value = !isDarkMode.value;
+				document.querySelector('html')?.setAttribute('data-bs-theme', isDarkMode.value ? 'dark' : 'light');
+			};
+
+			return { toggleDarkMode };
+		},
+	};
+</script>
 
 <template>
 	<nav>
 		<RouterLink to="/">Go to Home</RouterLink>
 		<RouterLink to="/about">Go to About</RouterLink>
+		<button @click="toggleDarkMode">Dark</button>
 	</nav>
 
 	<main>
