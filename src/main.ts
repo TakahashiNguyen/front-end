e99469d9fdd3da6ea3f29accbe000264e8bbf3ee
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2024-06-14 10:55:59 +0700

    load Bootstrap scripts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './scss/style.scss';
 import App from './App.vue';
 import { createMemoryHistory, createRouter } from 'vue-router';
 import BootstrapTest from './components/BootstrapTest.vue';
+import 'bootstrap';
 
 // Vue-router section
 const routes = [{ path: '/', component: BootstrapTest }];
