acc0c2c1f1c39d975a3c3457cdc1ff6f29ee881b
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2024-06-18 21:16:01 +0700

    update 5 files

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,23 @@ import vue from '@vitejs/plugin-vue';
 import fileListPlugin from './plugins/fileNameList';
 import fontsListing from './plugins/fontsListing';
 import appendType from './plugins/appendTypes';
+import { Plugin } from 'vite';
 
 // https://vitejs.dev/config/
+const plugins = [fileListPlugin, fontsListing, appendType];
+
 export default defineConfig({
-	plugins: [vue(), fileListPlugin(), fontsListing(), appendType()],
+	plugins: [vue()]
+		.concat(
+			plugins.map((i): Plugin => {
+				return { ...i(), apply: 'serve' };
+			}),
+		)
+		.concat(
+			plugins.map((i): Plugin => {
+				return { ...i(), apply: 'build' };
+			}),
+		),
 	build: {
 		outDir: 'dist',
 	},
