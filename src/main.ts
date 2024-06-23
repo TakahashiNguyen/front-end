7d4edbe3eb719719ff0c4a8039048a15f6f9aa2a
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2024-06-23 19:50:13 +0700

    Quick patch

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,13 @@ import BootstrapTest from './components/BootstrapTest.vue';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import HelloWorld from './components/HelloWorld.vue';
-import { Tolgee, DevTools, VueTolgee, LanguageDetector } from '@tolgee/vue';
+import {
+	Tolgee,
+	DevTools,
+	VueTolgee,
+	LanguageDetector,
+	TolgeeStaticData,
+} from '@tolgee/vue';
 import { FormatIcu } from '@tolgee/format-icu';
 import { availableLanguages } from './languages';
 
@@ -23,21 +29,41 @@ const router = createRouter({
 });
 
 // tolgee section
-const tolgee = Tolgee()
-	.use(DevTools())
-	.use(FormatIcu())
-	.use(LanguageDetector())
-	.init({
-		availableLanguages: availableLanguages,
-		defaultLanguage: availableLanguages[0],
+function genI18NImports(paths: string[][]): TolgeeStaticData {
+	return paths.reduce((acc, val: any) => {
+		return {
+			...acc,
+			[`${val[0]}:${val[1]}`]: () => import(`../src/i18n/${val[1]}/${val[0]}.json`),
+		};
+	}, {} as TolgeeStaticData);
+}
 
-		// for development
-		apiUrl: import.meta.env.VITE_TOLGEE_API_URL,
-		apiKey: import.meta.env.VITE_TOLGEE_API_KEY,
+function generatePermutations<T>(arr1: T[], arr2: T[]): T[][] {
+	const permutations: T[][] = [];
+	arr1.forEach(e => arr2.forEach(i => permutations.push([e, i])));
 
-		// import namespace
-		ns: ['tabs', 'advisory', 'lang'],
-	});
+	return permutations;
+}
+
+const namespace = ['tabs', 'advisory', 'lang'],
+	tolgee = Tolgee()
+		.use(DevTools())
+		.use(FormatIcu())
+		.use(LanguageDetector())
+		.init({
+			availableLanguages: availableLanguages,
+			defaultLanguage: availableLanguages[0],
+
+			// for development
+			apiUrl: import.meta.env.VITE_TOLGEE_API_URL,
+			apiKey: import.meta.env.VITE_TOLGEE_API_KEY,
+
+			// import namespace
+			ns: namespace,
+
+			// static
+			staticData: genI18NImports(generatePermutations(availableLanguages, namespace)),
+		});
 
 const pinia = createPinia();
 createApp(App).use(VueTolgee, { tolgee }).use(pinia).use(router).mount('body');
